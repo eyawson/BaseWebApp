537de13816e840c8814d8a77755ad2afd12e4da7
eyawson <ebow.yawson@gmail.com>
2020-03-31 00:46:14 -0400

    feat: add search functionality to app

--- project/js/main.js
+++ project/js/main.js
@@ -10,19 +10,24 @@ function myFunction() {
   );
 }
 
-$(document).ready(function() {
+/* $(document).ready(function () {
   getWeather();
-});
+}); */
 
-function getWeather() {
+function getWeather(searchQuery) {
   let url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Newark&units=imperial&appid=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&appid=" +
     apiKey;
   $.ajax(url, {
-    success: function(data) {
+    success: function (data) {
       console.log(data);
       $(".city").text(data.name);
       $(".temp").text(data.main.temp);
     }
   });
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
