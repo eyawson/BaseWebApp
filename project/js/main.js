e49a2dc686bec952931d54cf3e8333ba5557b86a
eyawson <ebow.yawson@gmail.com>
2020-05-14 01:52:02 -0400

    feat: add firebase sdk

--- project/js/main.js
+++ project/js/main.js
@@ -16,18 +16,47 @@ function myFunction() {
 
 function getWeather(searchQuery) {
   let url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&appid=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchQuery +
+    "&units=imperial&appid=" +
     apiKey;
   $.ajax(url, {
     success: function (data) {
       console.log(data);
       $(".city").text(data.name);
       $(".temp").text(data.main.temp);
-    }
+    },
   });
 }
 
 function searchWeather() {
   var searchQuery = $(".search").val();
+
   getWeather(searchQuery);
+}
+
+function handleSignin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user.email);
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 }
